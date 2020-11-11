d1c9bd0c6b857706605b84cc0a4f263f844e84dc
Dam <hoidam1128@gmail.com>
2020-11-11 12:04:43 +0800

    commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import json
+import os
 class web_app_ser:
     def __init__(self,name="Web Application Server"):
         self.activate=True
         self.name=name
-        self.size="small"
+        self.size="s"
         
     def upgrade_size(self,size):
         self.size=size
@@ -19,12 +21,13 @@ class database:
     def __init__(self,name="Database"):
         self.activate=True 
         self.name=name
-        self.size="small"
+        self.size="s"
         self.version="12c"
         self.available_version=[] 
         self.tested_version=[]
         self.risk=[]
-        self.encryption="NA"        
+        self.encryption="NA" 
+        self.duty="all"       
             
     def add_ava_ver(self,version):
         self.available_version.append(version)
@@ -62,9 +65,12 @@ class database:
         print("Tested Versions : " ,self.tested_version)
         print("Risks : " ,self.risk)
         print("Encryption : ",self.encryption)
+        print("Duty : " ,self.duty)
 
-
-def game_main():
+def game_main(patch_mapping,risk_mapping):
+    
+    # print(patch_mapping)
+    # print(risk_mapping)
     funds=1000
     round=1
     total_round=30 #default total round
@@ -82,16 +88,73 @@ def game_main():
             if msg=="End":
                 break
         
+
+
+        funds+=size_fund_func(db.size)
+        funds+=round_fund_func(db.size,round,total_round)
         round+=1
+        os.system('cls')
+
+def size_fund_func(size):
+    if size=="s":
+        return 250 
+    elif size=="m":
+        return 500 
+    elif size=="l": #L 
+        return 1000 
+    elif size=="xl":
+        return 2000 
+
+def round_fund_func(size,round,total_round):
+    punishment_ratio=250
+    if size=="s":
+        size_value=1
+    elif size=="m":
+        size_value=2
+    elif size=="l": #L 
+        size_value=3
+    elif size=="xl":
+        size_value=4
+    round_ratio=float(round/total_round)
+    if round_ratio<0.25:
+        size_value-=1
+    elif round_ratio<0.5:
+        size_value-=2
+    elif round_ratio<0.75:
+        size_value-=3
+    elif round_ratio<=1:
+        size_value-=4
+    
+    if size_value<0:
+        return punishment_ratio*size_value
+    else: return 0   
+
 
 def game_end():
+    
     print("Ended")
+
+
+
+
+
+
+
+
+
+
+
+
 
 while True:
     print("Database Game\nEnter \" 1 \" Start\nEnter \" 2 \" Exit")
     msg=int(input())
     if msg==1:
-        game_main()
+        with open('patch_mapping.json') as jf1:
+            patch_mapping=json.load(jf1)
+        with open('risk_mapping.json') as jf2:
+            risk_mapping=json.load(jf2)
+        game_main(patch_mapping,risk_mapping)
         game_end()
     elif msg==2:
         break
